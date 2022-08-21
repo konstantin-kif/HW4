4d4e0cc7e982859b0202a704c489facda0f45261
Konstantin <konstantin.kif@mail.ru>
2022-08-21 15:13:00 +0700

    add task3

---
+++ HW4/task3/Program.cs
@@ -0,0 +1,62 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов, выводит их на экран.
+// И ищет второй максимум (элемент меньше максимального, но больше всех остальных).
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string ReadValue = Console.ReadLine();
+    int result = int.Parse(ReadValue);
+    return result;
+}
+
+// Функция генерации массива
+int[] FillArray(int Length, int HighLimit, int LowLimit)
+{
+    int[] array = new int[Length];
+    int i = 0;
+    while (i < array.Length)
+    {
+        array[i] = new Random().Next(HighLimit, LowLimit);
+        i++;
+    }
+    return array;
+}
+
+// Функция ввывода массива
+void PrintArray(int[] array)
+{
+    int count = 0;
+    while (count < array.Length)
+    {
+        Console.Write($"{array[count]} \t");
+        count++;
+    }
+}
+
+int SecondMaxNumber(int[] arrayInput)
+{
+    const int zeroElement = 0;
+    const int secondElement = 1;
+    int numberMax = arrayInput[zeroElement];
+    int numberSecondMax = 0;
+    for (int index = secondElement; index < arrayInput.Length; index++)
+    {
+        if (arrayInput[index] >= numberMax)
+        {
+            numberSecondMax = arrayInput[index] >= numberMax ? numberMax : numberSecondMax;
+            numberMax = arrayInput[index];
+        }
+        else if (arrayInput[index] > numberSecondMax)
+        {
+            numberSecondMax = arrayInput[index];
+        }
+    }
+    return numberSecondMax;
+}
+
+int LenArray = Prompt("Введите число N, соответствующее количеству элементов в массиве: ");
+int LowLimit = Prompt("введите число, соответствующее нижней границы диапозона: ");
+int HighLimit = Prompt("                             верхней границы диапозона: ");
+int[] set = FillArray(LenArray, HighLimit, LowLimit);
+PrintArray(set);
+Console.WriteLine($"Второй максимум -> {SecondMaxNumber(set)}");
